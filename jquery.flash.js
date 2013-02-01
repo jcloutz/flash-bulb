c6093032882ca71abfd4837a90ea4e39c1f66c72
Jeremy Cloutier <jcloutier24@yahoo.com>
2013-01-31 23:56:37 -0500

    refactored for clearity

--- jquery.flash.js
+++ jquery.flash.js
@@ -1,88 +1,122 @@
-(function($){
+//Utility
+if(typeof Object.create !== 'function') {
+    Object.create = function( obj ) {
+        function F() {};
+        F.prototype = obj;
+        return new F();
+    }
+}
+
+(function($, window, document, undefined){
 
     var version = '0.1';
 
-    var timeout = null;
+    var Flash = {
+        init: function(elements, options, callback) {
+            var self = this;
+            self.elems = elements;
+            self.callback = callback;
+            self.timeout = null;
+            self.options = $.extend({}, $.fn.flash.options, options);
+        }, // end of init
 
-    $.flashIn = function(objects, speed, interval, callback) {
-        if(objects.length > 0) {
-            var i = 0;
-            objects.append('<div class="flash"></div>');
-            $('.flash').css({
-                'background-color': 'white',
-                'position': 'absolute',
-                'top': '0px',
-                'left': '0px',
-                'width': '100%',
-                'height': '100%'
-            }); // end flash.css;
-            process();
-        }
+        show: function() {
+            var self = this;
+            
+            if(self.elems.length > 0) {
+                var i = 0;
+                self.elems.append('<div class="flash"></div>');
+                $('.flash').css({
+                    'background-color': self.options.background,
+                    'position': 'absolute',
+                    'top': '0px',
+                    'left': '0px',
+                    'width': '100%',
+                    'height': '100%'
+                }); // end flash.css;
+                process();
+            }
 
-        function process() {
-            if(i < objects.length) {
-                var $element = $(objects[i]);
-                var flash = $element.children('.flash');
-                $element.show(1, function() {
-                    flash.animate({
-                        opacity: 0
-                    }, speed, function() {
-                        flash.remove();
+            function process() {
+                if(i < self.elems.length) {
+                    var $element = $(self.elems[i]);
+                    var flashBulb = $element.children('.flash');
+                    $element.show(1, function() {
+                        flashBulb.animate({
+                            opacity: 0
+                        }, self.options.fadeSpeed, function() {
+                            flashBulb.remove();
+                        });
+                    }); // end show
+                    timeout = setTimeout(process, self.options.interval);
+                }
+                else if($.isFunction(self.callback)) {
+                    self.callback.call(objects);
+                }
+                i++;
+            }
+            return self.elems;
+        }, // end of show
+
+        hide: function() {
+            var self = this;
+
+            if(self.elems.length > 0) {
+                var i = self.elems.length;
+                self.elems.append('<div class="flash"></div>');
+                $('.flash').css({
+                    'background-color': self.options.background,
+                    'position': 'absolute',
+                    'top': '0px',
+                    'left': '0px',
+                    'width': '100%',
+                    'height': '100%',
+                    'opacity': 0
+                }); // end flash.css
+                process();
+            }
+
+            function process() {
+                i--;
+                if(i >= 0) {
+                    var $element = $(self.elems[i]);
+                    var $flash = $element.children('.flash');
+                    $flash.animate({
+                        'opacity': 100
+                    }, self.options.initialFlash, function() {
+                        $element.animate({
+                            'opacity': 0
+                        }, self.options.fadeSpeed, function() {
+                            $element.hide();
+                        });
                     });
-                }); // end show
-                timeout = setTimeout(process, interval);
+                    timeout = setTimeout(process, self.options.interval);
+                }
             }
-            else if($.isFunction(callback)) {
-                callback.call(objects);
-            }
-            i++;
+            return self.elems;
+        } // end of hide
+    };
+
+    $.fn.flash = function(typeOrOptions, callback) {
+        var flash = Object.create(Flash);
+        var method = typeof(typeOrOptions) == String ? typeOrOptions : typeOrOptions.method || $.fn.flash.options.method;
+        flash.init(this, typeOrOptions, callback);
+
+
+        if(method == 'show') {
+            return flash.show();
+        } else if(method == 'hide') {
+            return flash.hide();
+        } else {
+            $.error('Method '+method+' does not exists on jQuery.flash');
         }
-        return objects
     }
 
-    $.flashOut = function(objects, speedIn, speedOut, interval, callback) {
-        if(objects.length > 0) {
-            var i = objects.length;
-            objects.append('<div class="flash"></div>');
-            $('.flash').css({
-                'background-color': 'white',
-                'position': 'absolute',
-                'top': '0px',
-                'left': '0px',
-                'width': '100%',
-                'height': '100%',
-                'opacity': 0
-            }); // end flash.css
-            process();
-        }
-
-        function process() {
-            i--;
-            if(i >= 0) {
-                var $element = $(objects[i]);
-                var $flash = $element.children('.flash');
-                $flash.animate({
-                    'opacity': 100
-                }, speedIn, function() {
-                    $element.animate({
-                        'opacity': 0
-                    }, speedOut, function() {
-                        $element.hide();
-                    });
-                });
-                timeout = setTimeout(process, interval);
-            }
-        }
-        return objects;
+    $.fn.flash.options = {
+        method: 'show',
+        initialFlash: 30,
+        fadeSpeed: 600,
+        interval: 200,
+        background: 'white',
     }
-
-    $.fn.flashIn = function(speed, interval, callback) {
-        $.flashIn(this, speed, interval, callback);
-        return this;
-    }
-
-    $.fn.flashOut = function(speedIn, speedOut, interval, callback) {
-        $.flashOut(this, speedIn, speedOut, interval, callback);
-        return this;
-    }
-})(jQuery);
+})(jQuery, window, document);
